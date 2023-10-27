b5e2a680256997be3191594ef981438f301b1c9b
AnthonySammuel <108153318+AnthonySammuel@users.noreply.github.com>
2023-10-27 16:03:32 -0300

    hhh

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ function tocaSomTim() {
     document.querySelector('#som_tecla_tim').play(); 
 }
 document.querySelector ('.tecla_tim').onclick= tocaSomTim;
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play(); 
+}
+document.querySelector ('.tecla_puff').onclick= tocaSomPuff;
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash).play(); 
+}
+document.querySelector ('.tecla_splash').onclick= tocaSomSplash;
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play(); 
+}
+document.querySelector ('.tecla_toim').onclick= tocaSomToim;
